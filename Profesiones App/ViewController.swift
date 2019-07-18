3a504f54c4f60f992d6e2d35d0246cf758746db4
Arlid Henao Rueda <arliid@hotmail.com>
2019-07-18 14:10:43 -0500

    Initial Commit

---
+++ Profesiones App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Profesiones App
+//
+//  Created by Arlid Henao Rueda on 7/18/19.
+//  Copyright © 2019 Arlid Henao Rueda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
