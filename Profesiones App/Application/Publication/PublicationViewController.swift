015796cb072f07df84563efa70bf817075403744
Arlid Henao Rueda <arliid@hotmail.com>
2019-07-22 14:02:07 -0500

    Se inicio la interfaz ( publication )

---
+++ Profesiones App/Application/Publication/PublicationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PublicationViewController.swift
+//  Profesiones App
+//
+//  Created by Arlid Henao Rueda on 7/22/19.
+//  Copyright © 2019 Arlid Henao Rueda. All rights reserved.
+//
+
+import UIKit
+
+class PublicationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
