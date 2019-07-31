df440f9e3db3091c0357fade228268d0c54a4ebd
Arlid Henao Rueda <arliid@hotmail.com>
2019-07-31 18:59:53 -0500

    Se finalizo el modulo (Local Notifications)

--- Profesiones App/Controllers/LocalNotificationsController.swift
+++ Profesiones App/Controllers/LocalNotificationsController.swift
@@ -10,5 +10,31 @@ import Foundation
 import UserNotifications
 
 class LocalNotificationsController {
-
+    init() {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, . badge]) { (success, error) in
+            if success {
+                self.addNotifications()
+            }
+        }
+    }
+    
+    func addNotifications(){
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "¿ Necesitas a un Profesional el día de hoy ?"
+        content.body = "Recuerda ingresar a la App para encontrar el Profesional que necesitas"
+        content.sound = UNNotificationSound.default
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 10.0, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "seconds", content: content, trigger: trigger)
+        
+        center.add(request) { (error) in
+            guard let error = error else {return}
+            print(error.localizedDescription)
+        }
+    }
+    
 }
