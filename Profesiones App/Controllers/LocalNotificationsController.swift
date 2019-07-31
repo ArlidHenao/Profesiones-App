35651cc222248cd6e08284dcd7b8cf1107590d93
Arlid Henao Rueda <arliid@hotmail.com>
2019-07-31 18:34:18 -0500

    Estructura de carpetas (Local Notifications)

---
+++ Profesiones App/Controllers/LocalNotificationsController.swift
@@ -0,0 +1,13 @@
+//
+//  LocalNotificationsController.swift
+//  Profesiones App
+//
+//  Created by Arlid Henao Rueda on 7/31/19.
+//  Copyright © 2019 Arlid Henao Rueda. All rights reserved.
+//
+
+import UIKit
+
+class LocalNotificationsController: NSObject {
+
+}
